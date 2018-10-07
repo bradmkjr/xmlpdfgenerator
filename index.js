7a3537ac12b6fbf0a1abc402effe5a7f88389738
Bradmkjr <brad@ucanbonline.net>
2018-10-06 21:47:09 -0400

    Added table of contents

--- index.js
+++ index.js
@@ -20,23 +20,80 @@ console.log(md5(url));
 const app = express();
 const port = process.env.PORT || 3000;
 
-function wwwServer(data){
-	console.log('www Server');
-	console.log(data.properties.property.length);
+function sort_unique(arr) {
+    arr = arr.sort(function (a, b) { return a*1 - b*1; });
+    var ret = [arr[0]];
+    for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
+        if (arr[i-1] !== arr[i]) {
+            ret.push(arr[i]);
+        }
+    }
+    return ret;
+}
 
-	app.get('/', (req, res) => {
-		const doc = new PDFDocument();
-		let filename = 'hot-sheet-9-28-2018';
-		filename = encodeURIComponent(filename) + '.pdf';
-		// Setting response to 'attachment' (download).
-		// If you use 'inline' here it will automatically open the PDF
-		// res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
-		res.setHeader('Content-type', 'application/pdf');
-		doc.y = 50;
-		doc.x = 50;
-		for (let i = 0; i < data.properties.property.length; i++) {
+function createPDF(data, res){
+	const doc = new PDFDocument({bufferPages: true});
+	let filename = 'hot-sheet-9-28-2018';
+	filename = encodeURIComponent(filename) + '.pdf';
+	// Setting response to 'attachment' (download).
+	// If you use 'inline' here it will automatically open the PDF
+	// res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
+	let counties = [];
+	let pages = [];
+	let listings = [];
+	for (let i = 0; i < data.properties.property.length; i++) {
+		counties.push(data.properties.property[i].county[0]);
+	}
+
+	// Skip 1st pages
+	doc.addPage();
+	doc.addPage();
+
+	// console.log(counties);
+
+	const sorted_counties = counties.filter(function(item, index){
+		return counties.indexOf(item) >= index;
+	}).sort();
+
+	// console.log(sorted_counties);
+	// doc.text(sorted_counties[0]+' county', 50, doc.page.height - 50, {
+	// 	    lineBreak: false
+	// 	});
+	doc.y = 50;
+	// doc.x = 50;
+
+	for (let i = 0; i < sorted_counties.length; i++) {
+		if( 0 !== i){
+			doc.addPage();
+			doc.moveDown();
+		}
+		doc.font('Helvetica-Bold').fontSize(18).text(sorted_counties[i]+' County');
+		doc.moveDown();
+		// doc.moveTo( doc.x, doc.y).lineTo(550, doc.y).stroke();
+		// doc.moveDown();
+		pages[i] = doc._root.document._pageBuffer.length;
+
+		for (let j = 0; j < data.properties.property.length; j++) {
+			// console.log(doc._root.document._pageBuffer.length);
 			// let i = 0;
-			const property = data.properties.property[i];
+			const property = data.properties.property[j];
+			if(property.county[0] !== sorted_counties[i]){
+				continue;
+			}
+			if(listings[i] !== undefined){
+				listings[i]++;
+			}else{
+				listings[i] = 1;
+			}
+			// if (doc.y > 600){
+			// 	doc.addPage();
+			// 	doc.text(sorted_counties[i]+' county', 50, doc.page.height - 50, {
+			// 	    lineBreak: false
+			// 	});
+			// 	doc.y = 50;
+			// 	doc.x = 50;
+
+			// }
 			// console.log(property);
 		    doc.font('Helvetica-Bold').fontSize(18).text(property.title);
 		    doc.font('Helvetica').fontSize(14).text(property.city+' '+property.state);
@@ -61,7 +118,7 @@ function wwwServer(data){
 		       .text(property.description,
 		       		 {  align: 'justify',
 		       		    width: 500,
-		       		    height: 100,
+		       		    height: 80,
 		       		    ellipsis: true
 		       		  });
 		    // console.log(property.images[0].image[0]);
@@ -73,13 +130,43 @@ function wwwServer(data){
 		    // doc.text('<!--- ---->');
 		    // doc.lineTo(400, 90).stroke();
 		    doc.moveDown();
-		    doc.moveTo( doc.x, doc.y).lineTo(550, doc.y).stroke();
-		    doc.moveDown();
+		    // doc.moveTo( doc.x, doc.y).lineTo(550, doc.y).stroke();
+		    // doc.moveDown();
 		}
+	}
 
-		doc.pipe(res);
-		doc.end();
+	// console.log(pages);
+	console.log(listings);
 
+	doc.switchToPage(0);
+	doc.y = 50;
+	// doc.x = 50;
+	doc.font('Helvetica-Bold').fontSize(18).text('Table of Contents');
+	for (let i = 0; i < sorted_counties.length; i++) {
+		if (doc.y > 700){
+			doc.switchToPage(1);
+			doc.y = 50;
+			// doc.x = 50;
+			doc.font('Helvetica-Bold').fontSize(18).text('Table of Contents Continued');
+		}
+		// doc.x = 50;
+		doc.font('Helvetica').fontSize(12).text(sorted_counties[i]+' County',{continued: true, width: 500, align: 'left'});
+		doc.font('Helvetica').fontSize(12).text('('+listings[i]+' Listings)',{continued: true, align: 'center'});
+		// doc.x = 400;
+		doc.text(pages[i],{align: 'right' }); // , lineGap: 10
+	}
+
+	doc.pipe(res);
+	doc.end();
+}
+
+function wwwServer(data){
+	console.log('www Server');
+	console.log(data.properties.property.length);
+
+	app.get('/', (req, res) => {
+		res.setHeader('Content-type', 'application/pdf');
+		createPDF(data, res);
 	});
 
 	app.listen(port, () => console.log(`PDF Server listening on port ${port}!`))
